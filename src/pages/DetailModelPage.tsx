231570352d2e77ec793d382d0b1a707f9b747f7e
Tan Nguyen <duytan17112003@gmail.com>
2023-10-26 09:09:46 +0700

    feat(default-model): add sub image, routing to default model, not yet design

--- src/pages/DetailModelPage.tsx
+++ src/pages/DetailModelPage.tsx
@@ -1,3 +1,13 @@
+import { useParams } from 'react-router-dom';
+import { useItemStore } from '@states/home';
+import { useEffect } from 'react';
 export function DetailModelPage() {
-  return <>Detail Model page</>;
+  const { itemData, getItemData } = useItemStore();
+  useEffect(() => {
+    getItemData();
+  }, []);
+  const params = useParams();
+  const modelId = params.id;
+  const model = itemData.find((item) => item.id.toString() === modelId);
+  return <div>{model?.name}</div>;
 }
