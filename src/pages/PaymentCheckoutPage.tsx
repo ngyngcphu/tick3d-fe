ba1d2db0d6084e653bd447e521d8408205281abc
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-10-27 23:25:05 +0700

    feat: complete cart

--- src/pages/PaymentCheckoutPage.tsx
+++ src/pages/PaymentCheckoutPage.tsx
@@ -12,6 +12,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import cashPaymentLogo from '@assets/cashpayment.webp';
 import momoPaymentLogo from '@assets/momopayment.png';
 import * as yup from 'yup';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const enum PaymentMethod {
   CASH,
@@ -61,11 +63,13 @@ function CheckoutForm() {
   };
 
   return (
-    <form className='flex flex-col gap-3 w-full max-w-screen-lg' onSubmit={handleSubmit(submit)}>
+    <form
+      className='flex flex-col gap-3 w-full max-w-screen-lg'
+      id='checkout-form'
+      onSubmit={handleSubmit(submit)}
+    >
       <div>
-        <Typography className='font-normal' variant='p' color='white'>
-          Phương thức thanh toán
-        </Typography>
+        <p className='text-black'>Phương thức thanh toán</p>
         <div>
           <Radio
             label={
@@ -79,17 +83,19 @@ function CheckoutForm() {
             crossOrigin=''
           />
         </div>
-        <Radio
-          label={
-            <div className='flex gap-2'>
-              <img src={momoPaymentLogo} className='h-6'></img>
-              <p>Momo</p>
-            </div>
-          }
-          value={PaymentMethod.MOMO}
-          name='paymentMethod'
-          crossOrigin=''
-        />
+        <div>
+          <Radio
+            label={
+              <div className='flex gap-2'>
+                <img src={momoPaymentLogo} className='h-7 bg-transparent'></img>
+                <p>Momo</p>
+              </div>
+            }
+            value={PaymentMethod.MOMO}
+            name='paymentMethod'
+            crossOrigin=''
+          />
+        </div>
         {errors.paymentMethod?.message && (
           <Typography color='red' variant='small'>
             {errors.paymentMethod?.message}{' '}
@@ -99,10 +105,10 @@ function CheckoutForm() {
 
       <div>
         <Input
-          className='text-white !rounded-none border-border-dark focus:border-white transition-all placeholder-shown:border-border-dark'
+          className='text-black !rounded-none border-border-dark focus:border-dark transition-all placeholder-shown:border-border-dark'
           labelProps={{
             className:
-              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-white peer-focus:text-white peer-focus:after:!border-white peer-placeholder-shown:text-border-dark',
+              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-gray peer-focus:text-gray peer-focus:after:!border-gray peer-placeholder-shown:text-border-dark',
             style: {
               top: '-0.45rem'
             }
@@ -126,10 +132,10 @@ function CheckoutForm() {
 
       <div>
         <Input
-          className='text-white !rounded-none border-border-dark focus:border-white transition-all placeholder-shown:border-border-dark'
+          className='text-black !rounded-none border-border-dark focus:border-black transition-all placeholder-shown:border-border-dark'
           labelProps={{
             className:
-              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-white peer-focus:text-white peer-focus:after:!border-white peer-placeholder-shown:text-border-dark',
+              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-black peer-focus:text-black peer-focus:after:!border-black peer-placeholder-shown:text-border-dark',
             style: {
               top: '-0.45rem'
             }
@@ -153,10 +159,10 @@ function CheckoutForm() {
 
       <div>
         <Input
-          className='text-white !rounded-none border-border-dark focus:border-white transition-all placeholder-shown:border-border-dark'
+          className='text-black !rounded-none border-border-dark focus:border-black transition-all placeholder-shown:border-border-dark'
           labelProps={{
             className:
-              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-white peer-focus:text-white peer-focus:after:!border-white peer-placeholder-shown:text-border-dark',
+              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-black peer-focus:text-black peer-focus:after:!border-black peer-placeholder-shown:text-border-dark',
             style: {
               top: '-0.45rem'
             }
@@ -180,10 +186,10 @@ function CheckoutForm() {
 
       <div>
         <Input
-          className='text-white !rounded-none border-border-dark focus:border-white transition-all placeholder-shown:border-border-dark'
+          className='text-black !rounded-none border-border-dark focus:border-black transition-all placeholder-shown:border-border-dark'
           labelProps={{
             className:
-              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-white peer-focus:text-white peer-focus:after:!border-white peer-placeholder-shown:text-border-dark',
+              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-black peer-focus:text-black peer-focus:after:!border-black peer-placeholder-shown:text-border-dark',
             style: {
               top: '-0.45rem'
             }
@@ -207,10 +213,10 @@ function CheckoutForm() {
 
       <div>
         <Textarea
-          className='text-white !rounded-none border-border-dark focus:border-white transition-all placeholder-shown:border-border-dark'
+          className='text-black !rounded-none border-border-dark focus:border-black transition-all placeholder-shown:border-border-dark'
           labelProps={{
             className:
-              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-white peer-focus:text-white peer-focus:after:!border-white peer-placeholder-shown:text-border-dark',
+              'before:rounded-tl-none after:rounded-tr-none text-border-dark before:border-border-dark after:border-border-dark peer-focus:before:!border-black peer-focus:text-black peer-focus:after:!border-black peer-placeholder-shown:text-border-dark',
             style: {
               top: '-0.45rem'
             }
@@ -230,56 +236,102 @@ function CheckoutForm() {
           </Typography>
         )}
       </div>
-
-      <Button
-        className='py-2 px-8 bg-transparent border rounded-none border-white capitalize w-fit font-normal text-base'
-        type='submit'
-      >
-        <div>Xác nhận</div>
-      </Button>
     </form>
   );
 }
 
 function OrderSummary() {
+  const [cart, setCart] = useState(
+    [] as { image: string; name: string; numberBought: number; price: number }[]
+  );
+  useEffect(() => {
+    async function fetchCart() {
+      setCart((await axios.get('http://localhost:3003/items')).data);
+    }
+
+    fetchCart();
+  }, []);
+
+  const productPrice = cart.reduce((acc, item) => acc + item.price * item.numberBought, 0);
+  const shippingFee = 30000;
   return (
     <>
       <div className='flex w-full'>
         <p className='flex-3'>Tổng phí sản phẩm</p>
-        <p className='flex-1 text-right'>10</p>
+        <p className='flex-1 text-right'>₫ {productPrice.toLocaleString('en-US')}</p>
       </div>
       <div className='flex w-full'>
         <p className='flex-3'>Phí vận chuyển</p>
-        <p className='flex-1 text-right'>10</p>
+        <p className='flex-1 text-right'>₫ {shippingFee.toLocaleString('en-US')}</p>
       </div>
       <hr />
       <div className='flex w-full'>
         <p className='flex-3'>Tổng chi phí</p>
-        <p className='flex-1 text-right'>10</p>
+        <p className='flex-1 text-right'>
+          ₫ {(productPrice + shippingFee).toLocaleString('en-US')}
+        </p>
       </div>
+
+      <Button
+        className='py-2 px-8 bg-transparent border rounded-none border-black capitalize w-fit font-normal text-base text-dark'
+        type='submit'
+        form='checkout-form'
+      >
+        <div>Xác nhận</div>
+      </Button>
     </>
+  );
+}
+
+function OrderDetails() {
+  const [cart, setCart] = useState(
+    [] as { image: string; name: string; numberBought: number; price: number }[]
+  );
+  useEffect(() => {
+    async function fetchCart() {
+      setCart((await axios.get('http://localhost:3003/items')).data);
+    }
+
+    fetchCart();
+  }, []);
+  return (
+    <div className='flex flex-col gap-5 my-5'>
+      {cart.map((item) => (
+        <div className='flex gap-4' key={item.name}>
+          <img src={item.image} alt={item.name} className='w-1/6' />
+          <div className='flex flex-row gap-10 w-full p-5'>
+            <Typography className='text-left w-1/2' variant='h5' color='dark'>
+              {item.name}
+            </Typography>
+            <p className='text-right w-1/5'>₫ {item.price.toLocaleString()}</p>
+            <p className='text-right w-1/5'>Sl: {item.numberBought.toLocaleString()}</p>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
 export function PaymentCheckoutPage() {
   return (
-    <div className='w-full lg:flex items-center justify-center bg-cover bg-no-repeat p-0 m-0'>
-      <Card className='bg-black/0 rounded-none sm:w-full lg:w-1/2' shadow={false}>
+    <div className='w-full lg:flex justify-center bg-cover bg-no-repeat p-0 m-0'>
+      <Card className='bg-white/100 rounded-none sm:w-full lg:w-1/2' shadow={false}>
         <CardBody className='flex flex-col gap-5'>
-          <Typography className='font-normal' variant='h2' color='white'>
+          <Typography className='font-normal' variant='h2' color='black'>
             Xác nhận đơn hàng
           </Typography>
           <CheckoutForm />
         </CardBody>
         <CardBody>
-          <Typography className='font-normal' variant='h2' color='white'>
+          <Typography className='font-normal' variant='h2' color='black'>
             Chi tiết đơn hàng
           </Typography>
+          <OrderDetails />
         </CardBody>
       </Card>
-      <Card className='bg-black/0 rounded-none sm:w-full lg:w-1/3' shadow={false}>
+      <Card className='bg-white/100 rounded-none sm:w-full lg:w-1/3' shadow={false}>
         <CardBody className='flex flex-col gap-5'>
-          <Typography className='font-normal' variant='h2' color='white'>
+          <Typography className='font-normal' variant='h2' color='black'>
             Hóa đơn
           </Typography>
           <OrderSummary />
