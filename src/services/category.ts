55bf7f7840dd998e6b032c673acb32d0e6cb3521
Thang <phamducthang2508@gmail.com>
2023-12-18 13:00:07 +0700

    feat(services): add services

--- src/services/category.ts
+++
@@ -1,5 +0,0 @@
-import { mockServer, invokeMockServer } from './common';
-
-export const categoryService = {
-  getAll: () => invokeMockServer<ItemData[]>(mockServer.get('/items'))
-};
--- src/types/checkout.d.ts
+++ src/types/checkout.d.ts
@@ -1,10 +1,24 @@
 type CheckoutForm = {
   paymentMethod: PaymentMethod;
+  total_price: number;
+  shipping_fee: number;
+  est_deli_time: string;
   district: string;
   ward: string;
   street: string;
   streetNo: string;
-  note?: string;
+  isPaid: boolean;
+  extra_note: string;
+};
+
+type PaypalInfo = {
+  intent: string;
+  paypalOrderId: string;
+};
+
+type OrderInfo = {
+  intent: string;
+  orderId: string;
 };
 
 type OrderData = {
